cc9d8c7ef8ed8f41b6c1cdbb6b7f0a6c32a65c8a
emp.justine <emp.justine@gmail.com>
2013-06-24 03:31:39 -0300

    Extra debug helpers.

--- lib/sinatra-debug.rb
+++ lib/sinatra-debug.rb
@@ -17,3 +17,24 @@ get '/debug/teapot' do
     "Refresh" => "Refresh: 20; http://www.ietf.org/rfc/rfc2324.txt"
   body "I'm a tea pot!"
 end
+
+
+get '/debug/params' do
+  params.to_json
+end
+
+
+get '/debug/request' do
+  obj = {
+    :env  => request.env,
+    :body => request.body,
+    :host => request.host,
+  }
+
+  erb :dicom_file, :locals => {
+    :title  => 'DEBUG',
+    :bootstrap => {:navbar => true},
+    :image => nil,
+    :metadata => JSON.pretty_generate(obj),
+  }
+end
